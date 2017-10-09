7136a2c10aea88397e64782b0c4ca06076d02ffe
Nick Wareing <nick.wareing@aranzmedical.com>
2017-10-10 11:29:34 +1300

    Spelling

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ export class App {
       { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
       { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
       { route: 'child-router', name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' },
-      { route: 'strava-activities', name: 'strava-activities', moduleId: 'strava-activities', nav: true, title: 'Nick\'s Strava Activiries' }
+      { route: 'strava-activities', name: 'strava-activities', moduleId: 'strava-activities', nav: true, title: 'Nick\'s Strava Activities' }
     ]);
 
     this.router = router;
